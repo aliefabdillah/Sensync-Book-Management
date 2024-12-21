d621ae0fb8dc2993b0f6f4d3d3a0c0d615a5c071
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 11:45:35 +0700

    chore: modify init model

--- api-book-management/src/models/books.js
+++ api-book-management/src/models/books.js
@@ -1,26 +1,55 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Model } from 'sequelize';
+import { DataTypes } from 'sequelize';
+import db from '../config/db.config.js';
 
-export default (sequelize, DataTypes) => {
-  class Books extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Books.init({
-    id: DataTypes.STRING,
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    year: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Books',
-    timestamps: true,
-  });
-  return Books;
-};
+// export default (sequelize, DataTypes) => {
+//   class Books extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   Books.init({
+//     id: DataTypes.STRING,
+//     title: DataTypes.STRING,
+//     author: DataTypes.STRING,
+//     year: DataTypes.STRING,
+//   }, {
+//     sequelize,
+//     modelName: 'Books',
+//     timestamps: true,
+//   });
+//   return Books;
+// };
+
+export const Books = db.define('Books', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  title: {
+    type: DataTypes.STRING,
+  },
+  author: {
+    type: DataTypes.STRING,
+  },
+  year: {
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+  },
+}, {
+  timestamps: true,
+})
