43027a85fa36e310427692fadcc26a0f0c9114f2
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 11:46:15 +0700

    feat: create controller books

---
+++ api-book-management/src/controllers/books.controller.js
@@ -0,0 +1,16 @@
+import booksService from '../services/books.service.js';
+import Response from '../utils/Response.js';
+import httpStatus from 'http-status'
+
+async function getAllBooks(req, res, next) {
+  try {
+    const bookResult = await booksService.getAll();
+    res.status(bookResult.statusCode).send(bookResult);
+  } catch (error) {
+    next(error)
+  }
+}
+
+export default {
+  getAllBooks
+}
