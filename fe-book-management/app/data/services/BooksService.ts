91e3584b3cbacc13d79da755b4b90012c4d5d780
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-22 01:40:02 +0700

    feat: create delete book service api

--- fe-book-management/app/data/services/BooksService.ts
+++ fe-book-management/app/data/services/BooksService.ts
@@ -74,4 +74,29 @@ export class BooksService {
         }
       });
   };
+
+  deleteBooks = (id: string) => {
+    return this.instance
+      .delete(`/${id}`,)
+      .then((res) => {
+        return res.data;
+      })
+      .catch(function (error) {
+        console.log(error)
+        if(error.response) {
+          const errorResponse = {
+            code: error.response.status,
+            message: error.response.statusText,
+          }
+          return errorResponse
+        } else {
+          const errorResponse = {
+            code: error.code,
+            message: error.message,
+            name: error.name
+          }
+          return errorResponse;
+        }
+      });
+  }
 }
