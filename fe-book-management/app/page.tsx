3f71c66aebf13bba671e46d86cf1dd10669d6a1a
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 17:13:44 +0700

    feat: change to basepage

--- fe-book-management/app/page.tsx
+++ fe-book-management/app/page.tsx
@@ -1,16 +1,10 @@
+import BasePage from "./components/BasePage";
 import ListCard from "./components/ListCard";
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center">
-      <div className="flex flex-col  bg-pink-200 w-2/3 items-center py-6">
-        <p className="text-4xl text-center font-lexend_deca font-extrabold mt-4">
-          📚 Book Management System
-        </p>
-        <div className="bg-green-400 w-full mt-8">
-          <ListCard />
-        </div>
-      </div>
-    </div>
+    <BasePage>
+      <ListCard />
+    </BasePage>
   );
 }
