e1b5534a1ef1da8b18513427374691c920c11c0e
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 23:22:51 +0700

    feat: create loading ui component

---
+++ fe-book-management/app/components/Loader/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <>
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-blue-400 border-t-transparent"></div>
+    </>
+  );
+}
