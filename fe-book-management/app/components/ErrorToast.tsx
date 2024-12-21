0c615fb13028863f23350940f6b35765d908ce99
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 23:23:17 +0700

    feat: create error toast component

---
+++ fe-book-management/app/components/ErrorToast.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { ApiError } from "../types/ApiError";
+
+export default function ErrorToast({
+  error,
+  isOpen,
+  onClose,
+}: {
+  error: ApiError;
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | null = null;
+    if (isOpen) {
+      timeoutId = setTimeout(() => {
+        onClose();
+      }, 10000); // 10 detik
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId); // Membersihkan timeout jika komponen di-unmount sebelum timeout tercapai
+      }
+    };
+  }, [isOpen, onClose]);
+
+  if (!error.message) return null;
+  return (
+    <>
+      {isOpen && (
+        <div className="toast toast-end">
+          <div className="alert alert-error">
+            <span className="text-white font-lexend_deca">{`${error.code}: ${error.message}`}</span>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
