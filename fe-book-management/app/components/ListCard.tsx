78a8ecc8ffe4861f0ea49541ffe39f89f99b7880
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 18:27:54 +0700

    feat: create button add state

--- fe-book-management/app/components/ListCard.tsx
+++ fe-book-management/app/components/ListCard.tsx
@@ -1,35 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Link from "next/link";
+import FormBook from "./FormBook";
 
 export default function ListCard() {
+  const [isFormShow, setIsFormShow] = useState(false);
+
+  const handleBtnAddState = (state: boolean) => {
+    setIsFormShow(state);
+  };
+
   return (
-    <div>
-      <div className="flex justify-end px-4">
-        <button className="btn btn-success btn-md text-white">
-          <AddIcon style={{ fontSize: 35 }} />
-          <p className="font-poppins text-lg">Add</p>
-        </button>
-      </div>
-      <div className="grid grid-cols-3 gap-4 mt-4 px-4">
-        {Array.from({ length: 5 }, (_, index) => (
-          <Link key={index} href={`/details/${index}`}>
-            <div
-              key={index}
-              className="
+    <div className="w-full">
+      {isFormShow ? (
+        <FormBook getIsFormShowingState={handleBtnAddState}/>
+      ) : (
+        <>
+          <div className="flex justify-end px-4">
+            <button
+              onClick={() => handleBtnAddState(true)}
+              className="btn btn-success btn-md text-white"
+            >
+              <AddIcon style={{ fontSize: 35 }} />
+              <p className="font-poppins text-lg">Add</p>
+            </button>
+          </div>
+          <div className="grid grid-cols-3 gap-4 mt-4 px-4">
+            {Array.from({ length: 5 }, (_, index) => (
+              <Link key={index} href={`/details/${index}`}>
+                <div
+                  key={index}
+                  className="
               card lg:card-side bg-gray-200 shadow-xl 
               transnform transition-all duration-300 ease-in-out
               hover:scale-105 hover:shadow-2xl hover:cursor-pointer hover:text-white hover:bg-info
             "
-            >
-              <div className="card-body">
-                <h2 className="card-title">{`New movie ${index} is released!`}</h2>
-                <p>Click the button to watch on Jetflix app.</p>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
+                >
+                  <div className="card-body">
+                    <h2 className="card-title">{`New movie ${index} is released!`}</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
