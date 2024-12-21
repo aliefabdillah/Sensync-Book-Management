f826e59d9121d56de1ee17979e29650952bd1d67
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 17:13:59 +0700

    feat: make link to details page

--- fe-book-management/app/components/ListCard.tsx
+++ fe-book-management/app/components/ListCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
+import Link from "next/link";
 
 export default function ListCard() {
   return (
-    <>
+    <div>
       <div className="flex justify-end px-4">
         <button className="btn btn-success btn-md text-white">
           <AddIcon style={{ fontSize: 35 }} />
@@ -12,21 +13,23 @@ export default function ListCard() {
       </div>
       <div className="grid grid-cols-3 gap-4 mt-4 px-4">
         {Array.from({ length: 5 }, (_, index) => (
-          <div
-            key={index}
-            className="
+          <Link key={index} href={`/details/${index}`}>
+            <div
+              key={index}
+              className="
               card lg:card-side bg-gray-200 shadow-xl 
               transnform transition-all duration-300 ease-in-out
               hover:scale-105 hover:shadow-2xl hover:cursor-pointer hover:text-white hover:bg-info
             "
-          >
-            <div className="card-body">
-              <h2 className="card-title">{`New movie ${index} is released!`}</h2>
-              <p>Click the button to watch on Jetflix app.</p>
+            >
+              <div className="card-body">
+                <h2 className="card-title">{`New movie ${index} is released!`}</h2>
+                <p>Click the button to watch on Jetflix app.</p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
-    </>
+    </div>
   );
 }
