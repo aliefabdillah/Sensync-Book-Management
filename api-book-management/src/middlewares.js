960ac3cbb074a182d58dc8e504a7032bd05ad749
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-21 11:44:46 +0700

    chore: modify response error middlewares

--- api-book-management/src/middlewares.js
+++ api-book-management/src/middlewares.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line import/no-unresolved
+import httpStatus from 'http-status';
+import Response from './utils/Response.js';
+
 function notFound(req, res, next) {
   res.status(404);
   const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
@@ -7,12 +11,21 @@ function notFound(req, res, next) {
 /* eslint-disable no-unused-vars */
 function errorHandler(err, req, res, next) {
   /* eslint-enable no-unused-vars */
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
-  });
+  const { statusCode = httpStatus.INTERNAL_SERVER_ERROR, message } = err;
+
+  res.locals.errorMessage = err.message;
+
+  const response = {
+    code: statusCode,
+    message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack }),
+  };
+
+  if (process.env.NODE_ENV === 'development') {
+    console.error('Error:', err);
+  }
+
+  res.status(statusCode).send(response);
 }
 
 export default {
