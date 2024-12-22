3d29e56851dbdc2ce885513329088813536c67a6
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-22 12:24:27 +0700

    feat: modify parameter

--- fe-book-management/app/components/EmptyPage.tsx
+++ fe-book-management/app/components/EmptyPage.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
 import React from "react";
 
-export default function EmptyPage({ item }: { item: string }) {
+export default function EmptyPage({
+  item,
+  image,
+  size,
+}: {
+  item: string;
+  image: string;
+  size: number;
+}) {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
-      <Image src={"/no-results.png"} alt="No Results" width={250} height={250} unoptimized/>
-      <p className="text-center font-poppins text-2xl font-semibold">{item} Not Found!</p>
+      <Image src={image} alt="404" width={size} height={size} unoptimized priority/>
+      <p className="text-center font-poppins text-2xl font-semibold">
+        {item} Not Found!
+      </p>
     </div>
   );
 }
