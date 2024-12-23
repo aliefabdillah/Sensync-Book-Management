ca73012d333ddbdd7dc312d966115e695bd912f5
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-23 09:08:54 +0700

    chore: move to env file

--- fe-book-management/app/data/services/index.ts
+++ fe-book-management/app/data/services/index.ts
@@ -1,3 +1,3 @@
 import { BooksService } from "./BooksService";
 
-export const booksService = new BooksService(`http://localhost:3001/api/v1/books/`);
+export const booksService = new BooksService(`${process.env.API_URL}/books/`);
